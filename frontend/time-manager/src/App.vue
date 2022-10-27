747f39e16471af22976a6e8f94a6e7be820493bb
dreuxfl <florent.dreux@epitech.eu>
2022-10-28 00:08:05 +0200

    Register functionnal, creates user and passes id to App vue

--- frontend/time-manager/src/App.vue
+++ frontend/time-manager/src/App.vue
@@ -1,10 +1,54 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <UserLogin/>
+    <q-header elevated class="bg-primary text-white">
+      <q-toolbar class="row justify-between">
+        <q-toolbar-title>
+          <q-avatar>
+            <img src=".\assets\Logo_Team.png">
+          </q-avatar>
+          Clockorico !
+        </q-toolbar-title>
 
-    <q-page-container>
-      <router-view />
-      <div class="q-a-md row items-start q-gutter-md justify-center align-center ">
+        <q-btn-dropdown  color="primary" label="Select chart to display">
+        <q-list>
+          <q-item clickable v-close-popup @click="onItemClick">
+            <q-item-section>
+              <q-item-label>Chart 1</q-item-label>
+            </q-item-section>
+          </q-item>
+
+          <q-item clickable v-close-popup @click="onItemClick">
+            <q-item-section>
+              <q-item-label>Chart 2</q-item-label>
+            </q-item-section>
+          </q-item>
+
+          <q-item clickable v-close-popup @click="onItemClick">
+            <q-item-section>
+              <q-item-label>Chart 3</q-item-label>
+            </q-item-section>
+          </q-item>
+        </q-list>
+        </q-btn-dropdown>
+
+        <q-space />
+
+        <q-btn color="primary" icon-right="person" label="Sign in" @click="toggleRightDrawer" />
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer v-model="rightDrawerOpen" side="right" behavior="mobile" elevated class="flex justify-center">
+      <UserLogin @create-user-event="setUserId"/>
+    </q-drawer>
+
+    <q-page-container >
+
+      <div v-if="userId === null" class="flex justify-center column">
+        <q-space />
+        <q-btn color="primary" icon-right="person" label="Sign in" @click="toggleRightDrawer" />
+        <q-space />
+      </div>
+      <div v-else class="q-a-md row items-start q-gutter-md justify-center align-center ">
         <clock-work userId=1 />
         <working-time/>
         <working-times/>
@@ -20,7 +64,7 @@
   import ClockWork from "./components/ClockWork";
   import WorkingTime from "./components/WorkingTime";
   import WorkingTimes from "./components/WorkingTimes";
-  import UserLogin from "./components/UserLogin.vue";
+  import UserLogin from "./components/User.vue";
 
   export default {
     name: 'LayoutDefault',
@@ -32,9 +76,19 @@
       UserLogin
     },
 
-    setup () {
+    methods:{
+      setUserId(payload){
+          this.userId = payload.id;
+          console.log(this.userId);
+      },
+        toggleRightDrawer() {
+        this.rightDrawerOpen = !this.rightDrawerOpen;
+      },
+    },
+    data() {
       return {
-        leftDrawerOpen: ref(false)
+        rightDrawerOpen : ref(false),
+        userId: null,
       }
     }
   }
