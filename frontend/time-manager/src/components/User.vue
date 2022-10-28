2fc50eb2cc04c3bffc98ecb4d4aaac0ebac0fb84
dreuxfl <florent.dreux@epitech.eu>
2022-10-28 16:42:01 +0200

    User login axios OK + email edit OK

--- frontend/time-manager/src/components/User.vue
+++ frontend/time-manager/src/components/User.vue
@@ -5,9 +5,9 @@
 
       <div v-if="userId==null">
         
-        <q-form v-if="isLogin" @login="onLogin" @reset="resetLogin" class="q-gutter-md">
-          <q-input filled v-model="email" label="Email" hint="xxx@yyy" lazy-rules
-            :rules="[val => val && val.length > 0 || 'Email is required']" />
+        <q-form v-if="isLogin" @submit="onLogin" @reset="clearLoginFields" class="q-gutter-md">
+          <q-input filled v-model="username" label="Username" hint="xxx@yyy" lazy-rules
+            :rules="[val => val && val.length > 0 || 'Username is required']" />
 
           <q-input filled v-model="password" label="Password" :type="isPwd ? 'password' : 'text'" lazy-rules
             :rules="[val => val && val.length > 0 || 'Password is required']">
@@ -18,7 +18,7 @@
           <q-checkbox v-model="customModel" label="Remember username and password ?" true-value="yes" false-value="no" />
 
           <div class="flex row q-gutter-sm">
-            <q-btn class="col" label="Login" type="login" color="primary" />
+            <q-btn class="col" label="Log in" type="submit" color="primary" />
             <q-btn class="col" label="Erase" type="reset" color="negative" />
             <q-btn class="col" label="Register" color="primary" flat @click="toggleLogin" />
 
@@ -26,9 +26,9 @@
         </q-form>
 
         <q-form v-else @submit="onRegister" @reset="clearRegisterFields" class="q-gutter-md">
-          <q-input filled v-model="email1" label="Email" hint="xxx@yyy" lazy-rules
+          <q-input filled v-model="email" label="Email" hint="xxx@yyy" lazy-rules
             :rules="[val => val && val.length > 0 || 'Email is required']" />
-          <q-input filled v-model="username" label="Username" lazy-rules
+          <q-input filled v-model="username1" label="Username" lazy-rules
             :rules="[val => val && val.length > 0 || 'Username is required']" />
             
           <q-input filled v-model="password1" label="Password" :type="isPwd1 ? 'password' : 'text'" lazy-rules
@@ -47,7 +47,7 @@
           <q-toggle v-model="accept" label="I accept the license and terms" />
 
           <div class="flex row q-gutter-sm" >
-            <q-btn class="col" label="Submit" type="submit" color="primary" />
+            <q-btn class="col" label="Register" type="submit" color="primary" />
             <q-btn class="col" label="Reset" type="reset" color="negative" />
             <q-btn class="col" label="Login" color="primary" flat @click="toggleLogin" />
 
@@ -56,7 +56,25 @@
         
       </div>
 
-      <q-btn v-else color="negative" icon-right="logout" label="Disconnect" @click="onDisconnect" />
+      <div v-else-if="connectedUserData!==null" class="flex column align-center">
+        <div class="text-h5">Connected as {{connectedUserData.username}}</div>
+
+        <q-form v-if="isLogin" class="q-gutter-md">
+
+          <q-input filled v-model="connectedUserData.email" label="Email" lazy-rules
+            :rules="[val => val && val.length > 0 || 'Email is required']" >
+            <template v-slot:after>
+              <q-btn class="col" label="Edit" color="primary" @click="onUpdate"/>
+            </template>
+          </q-input>
+   
+        </q-form>
+
+        <q-space/>
+
+        <q-btn color="negative" icon-right="logout" label="Disconnect" @click="onDisconnect" />
+
+      </div>
 
       <q-space />
     </div> 
@@ -71,22 +89,38 @@ import { useQuasar } from 'quasar';
 
 export default {
   name: 'UserComponent',
+  setup () {
+    const $q = useQuasar()
+
+    return {
+      showNotif (positive, message) {
+        $q.notify({
+          color: (positive ? "positive" : "warning"),
+          textColor: 'white',
+          icon: (positive ? "cloud_done" : "warning"),
+          message: message
+        })
+      }
+    }
+  },
   data() {
     return {
+      connectedUserData : null,
+      userId : null,
+
       isLogin : true,
-      $q : useQuasar(),
       isPwd : ref(true),
       isPwd1 : ref(true),
       isPwd2 : ref(true),
-      email : ref(""),
-      email1 : ref(""),
       username : ref(""),
+      email : ref(""),
+      username1 : ref(""),
       password : ref(""),
       password1 : ref(""),
       password2 : ref(""),
       accept : ref(false),
-      customModel : ref(false),
-      userId : null
+      customModel : ref(false)
+      
     }
   },
   methods:{
@@ -94,57 +128,71 @@ export default {
       this.isLogin = !this.isLogin;
     },
     clearRegisterFields(){
-      this.email1 = null;
+      this.email = null;
       this.password1 = null;
       this.password2 = null;
       this.accept = false;
     },
-    resetLogin() {
+    clearLoginFields() {
       this.email = null;
       this.password = null;
       this.customModel = false;
     },
     onRegister() {
-      if (this.accept !== true) {
-        this.$q.notify = {
-          color: 'red-5',
-          textColor: 'white',
-          icon: 'warning',
-          message: 'You need to accept the license and terms first'
-        }
-      }
+      if (this.accept) this.showNotif (false, 'You need to accept the license and terms first')
+        
       else {
-        this.$q.notify= {
-          color: 'green-4',
-          textColor: 'white',
-          icon: 'cloud_done',
-          message: 'Submitted'
-        }
-
-        this.createUser(this.email1, this.username, this.password1);
+        UserService.postUser(this.email, this.username1, this.password1).then((response)=>{
+          try {
+            this.showNotif (true, 'Registration complete. You are now logged in')
+            this.userId = response.data.data.id
+            this.$emit('user-create-event', {id: response.data.data.id})
+            this.clearRegisterFields();
+            this.updateConnectedUserData()
+          } catch (e) {
+            console.log(e);
+          }
+        }).catch(e => console.log(e));      
       }
     },
-
-    createUser(email, username, password){
-      UserService.postUser(email, username, password).then((response)=>{
+    onLogin() {
+      UserService.login(this.username, this.password).then((response)=>{
         try {
           this.userId = response.data.data.id
-          this.$emit('create-user-event', {id: response.data.data.id})
-          this.clearRegisterFields();
+          this.$emit('user-login-event', {id: response.data.data.id})
+          this.clearLoginFields();
+          this.updateConnectedUserData()
+          this.showNotif (true, 'You are now logged in')
         } catch (e) {
           console.log(e);
         }
-      }).catch(e => console.log(e));
+      }).catch(e => {
+        console.log(e);
+        this.showNotif (false, 'Invalid credentials');
+      });
+    },
+    onUpdate(){
+      console.log("onupdate pls")
+      this.updateUser(this.connectedUserData.email, this.connectedUserData.username, this.userId);
+    },
+    onDisconnect(){
+      this.userId = null;
+      this.updateConnectedUserData()
+      this.$emit('create-user-event', {id: null});
     },
 
-    updateUser(email, username, password, userId){
-      UserService.putUser(email, username, password, userId).then((response)=>{
+    updateUser(email, username, userId){
+      UserService.putUser(email, username, userId).then((response)=>{
         try {
-          console.log(response)
+          this.updateConnectedUserData()
+          this.showNotif (true, `Email updated to ${email}`);
         } catch (e) {
           console.log(e);
         }
-      }).catch(e => console.log(e));
+      }).catch(e => {
+        console.log(e)
+        this.showNotif (false, `Update error`);
+      });
     },
 
     getUser(userId){
@@ -166,16 +214,14 @@ export default {
         }
       }).catch(e => console.log(e));
     },
-    
-    onLogin() {
-      //TODO
-    },
+    updateConnectedUserData(){
+      if(this.userId) UserService.getUser(this.userId).then(response =>  this.connectedUserData = response.data.data )
+      else this.connectedUserData = null;
 
-    onDisconnect(){
-      this.userId = null;
-      this.$emit('create-user-event', {id: null});
     }
-    
+  },
+  created: function () {
+    this.updateConnectedUserData()
   }
 }
 </script>
