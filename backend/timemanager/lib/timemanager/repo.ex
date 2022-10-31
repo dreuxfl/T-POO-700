69c5bb5ec8209033813d13877c0c0f5138cd448e
dreuxfl <florent.dreux@epitech.eu>
2022-10-31 16:44:53 +0100

    Dev Environment setup

--- backend/timemanager/lib/timemanager/repo.ex
+++ backend/timemanager/lib/timemanager/repo.ex
@@ -2,4 +2,13 @@ defmodule Timemanager.Repo do
   use Ecto.Repo,
     otp_app: :timemanager,
     adapter: Ecto.Adapters.Postgres
+    def init(_, config) do
+      config = config
+        |> Keyword.put(:username, System.get_env("PGUSER") || "postgres")
+        |> Keyword.put(:password, System.get_env("PGPASSWORD") || "password")
+        |> Keyword.put(:database, System.get_env("PGDATABASE") || "time_manager_dev")
+        |> Keyword.put(:hostname, System.get_env("PGHOST") || "localhost")
+        |> Keyword.put(:port, System.get_env("PGPORT") || 5432)
+      {:ok, config}
+    end
 end
