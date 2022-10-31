42f6d3962b6a716b94ddeabc9656f4bb1c993cb0
Wael Fezari <wael.fezari@epitech.eu>
2022-10-31 16:42:41 +0100

    Update repo.ex

--- backend/timemanager/lib/timemanager/repo.ex
+++ backend/timemanager/lib/timemanager/repo.ex
@@ -2,4 +2,36 @@ defmodule Timemanager.Repo do
   use Ecto.Repo,
     otp_app: :timemanager,
     adapter: Ecto.Adapters.Postgres
+
+  def init(_, config) do
+
+    config = config
+
+
+
+             |> Keyword.put(:username, System.get_env("PGUSER") || "postgres")
+
+
+
+             |> Keyword.put(:password, System.get_env("PGPASSWORD") || "password")
+
+
+
+             |> Keyword.put(:database, System.get_env("PGDATABASE") || "timemanager_dev")
+
+
+
+             |> Keyword.put(:hostname, System.get_env("PGHOST") || "localhost")
+
+
+
+             |> Keyword.put(:port, System.get_env("PGPORT") || 5432)
+
+
+
+    {:ok, config}
+
+
+
+  end
 end
