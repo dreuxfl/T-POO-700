5577150ed5562ed6499af277767eb3cb1f62d018
Max13010 <maxence.gassier@epitech.eu>
2022-11-08 16:04:37 +0100

    Upfate catch phrase

--- mobile/timemanager/screens/StartScreen.js
+++ mobile/timemanager/screens/StartScreen.js
@@ -4,16 +4,19 @@ import Logo from '../components/Logo'
 import Header from '../components/Header'
 import Button from '../components/Button'
 import Paragraph from '../components/Paragraph'
-import {StyleSheet} from "react-native";
+import { StyleSheet} from "react-native";
 
 export default function StartScreen({ navigation }) {
+    const [random, setRandom] = useState(SampleNameArray[Math.random(SampleNameArray.length)])
+
+    var SampleNameArray = ["it's finger clockin' good", "Clock me like one of your french girls", "Los clockos hermanos", "La ferme aux clockodiles", "I'm clockin'it", "Clock it like its hot", "Hit me with your best clock", "Parce que nous le clockons bien", "Just clock it", "Des clocks oui, mais des clockorico", "Yves Clocké– Se clocker chaque jour..", "Clock comme si tu devais démissionner demain.", "Suck my clock", "Travail comme si tu devais clocker toujours", "C'est fort en clockocolat", "C’est moche, mais ça marche.", "Clocking people", "Clockorico en avant les clocks in"];
     return (
         <Background>
             <Logo />
             <Header>CLOCKORICO!</Header>
-            <Paragraph>
-                *Import a catch phrase (Maxence)*
-            </Paragraph>
+            <View style={styles.MainContainer}>
+                <Text>{random}</Text>
+            </View>
             <Button
                 mode="contained"
                 onPress={() => navigation.navigate('Login')}
@@ -29,3 +32,17 @@ export default function StartScreen({ navigation }) {
         </Background>
     );
 }
+const styles = StyleSheet.create({
+
+    MainContainer: {
+        flex: 1,
+        margin: 10
+
+    },
+
+    TextStyle: {
+        fontSize: 25,
+        textAlign: 'center'
+    }
+
+});
