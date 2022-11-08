59afeceedf89059e8a927aa2bc50a9cf36db3993
Max13010 <maxence.gassier@epitech.eu>
2022-11-08 11:40:08 +0100

    Signup update

--- mobile/timemanager/screens/Signup.js
+++ mobile/timemanager/screens/Signup.js
@@ -15,8 +15,31 @@ export default class Signup extends Component {
 
     render() {
         return (
-            <View>
-                <Text>Hello SIGNUP</Text>
+            <View>              
+                <BackButton goBack={navigation.goBack} />
+            <Logo />
+            <Header>Hello SIGNUP</Header>
+            <TextInput
+                label="Email"
+                returnKeyType="next"
+                value={email.value}
+                onChangeText={(text) => setEmail({ value: text, error: '' })}
+                error={!!email.error}
+                errorText={email.error}
+                autoCapitalize="none"
+                autoCompleteType="email"
+                textContentType="emailAddress"
+                keyboardType="email-address"
+            />
+            <TextInput
+                label="Password"
+                returnKeyType="done"
+                value={password.value}
+                onChangeText={(text) => setPassword({ value: text, error: '' })}
+                error={!!password.error}
+                errorText={password.error}
+                secureTextEntry
+            />
             </View>
         )
     }
