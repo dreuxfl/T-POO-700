6dfe12f031adb03f390872eac5db5894aec1de55
Max13010 <maxence.gassier@epitech.eu>
2022-11-08 16:08:37 +0100

    Update Catch phrase v1.1 tg seb

--- mobile/timemanager/screens/StartScreen.js
+++ mobile/timemanager/screens/StartScreen.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Background from '../components/Background'
 import Logo from '../components/Logo'
 import Header from '../components/Header'
