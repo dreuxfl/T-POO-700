ebe3e5788c43f13eb0ce9058eed397789a850c24
Max13010 <maxence.gassier@epitech.eu>
2022-11-08 13:35:31 +0100

    Update Signup.js v1

--- mobile/timemanager/screens/Signup.js
+++ mobile/timemanager/screens/Signup.js
@@ -1,24 +1,56 @@
 import React, {Component} from 'react';
 import {Text, View} from 'react-native';
+import React, { useState } from 'react'
+import { TouchableOpacity, StyleSheet, View } from 'react-native'
+import { Text } from 'react-native-paper'
+import Button from '../components/Button'
+import TextInput from '../components/TextInput'
+import { theme } from '../core/Theme'
+import { EmailValidator } from '../helpers/EmailValidator'
+import { PasswordValidator } from '../helpers/PasswordValidator'
+import { UsernameValidator } from '../helpers/PasswordValidator'
+import Background from "../components/Background";
+import BackButton from "../components/BackButton";
+import Header from "../components/Header";
+import Logo from "../components/Logo";
 
-export default class Signup extends Component {
-    state = {};
+export default function Signup({ navigation }) {
+    const [email, setEmail] = useState({ value: '', error: '' })
+    const [password, setPassword] = useState({ value: '', error: '' })
+    const [username, setUsername] = useState({ value: '', error: '' })
 
-    constructor(props) {
-        super(props)
+    const onSignupPressed = () => {
+        const emailError = EmailValidator(email.value)
+        const passwordError = PasswordValidator(password.value)
+        const usernameError = UsernameValidator(username.value)
+        if (emailError || passwordError || usernameError) {
+            setEmail({ ...email, error: emailError })
+            setPassword({ ...password, error: passwordError })
+            setUsername({ ...username, error: usernameError })
+            return
+        }
+        navigation.reset({
+            index: 0,
+            routes: [{ name: 'Dashboard' }],
+        })
     }
-
-    static propTypes = {};
-
-    componentDidMount() {
-    }
-
-    render() {
         return (
             <View>              
                 <BackButton goBack={navigation.goBack} />
             <Logo />
             <Header>Hello SIGNUP</Header>
+            <TextInput
+                label="Username"
+                returnKeyType="next"
+                value={username.value}
+                onChangeText={(text) => setUsername({ value: text, error: '' })}
+                error={!!username.error}
+                errorText={username.error}
+                autoCapitalize="none"
+                autoCompleteType="username-new"
+                textContentType="username"
+                keyboardType="default"
+            />
             <TextInput
                 label="Email"
                 returnKeyType="next"
@@ -40,8 +72,10 @@ export default class Signup extends Component {
                 errorText={password.error}
                 secureTextEntry
             />
+            <Button mode="contained" onPress={onSignupPressed}>
+                Sign up
+            </Button>
             </View>
         )
     }
-}
 
