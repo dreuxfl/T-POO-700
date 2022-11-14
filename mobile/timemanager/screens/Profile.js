9e61c1bbeeb9a8e6f7f1187c59a442780a124718
Sébastien Cartoux <sebastien.cartoux@epitech.eu>
2022-11-14 16:10:00 +0100

    Work in progress

--- mobile/timemanager/screens/Profile.js
+++ mobile/timemanager/screens/Profile.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { TouchableOpacity, StyleSheet } from 'react-native';
+import {TouchableOpacity, StyleSheet, ToastAndroid} from 'react-native';
 import { Text } from 'react-native-paper';
 import TextInput from '../components/TextInput';
 import { theme } from '../core/Theme';
@@ -9,47 +9,74 @@ import Header from "../components/Header";
 import { UsernameValidator } from '../helpers/UsernameValidator';
 import { EmailValidator } from '../helpers/EmailValidator';
 import Ionicons from "react-native-vector-icons/Ionicons";
+import UserService from "../services/UserService";
 
 export default function Profile() {
-    const [email, setEmail] = useState({ value: '', error: '' });
-    const [password, setPassword] = useState({ value: '', error: '' });
+    const [username, setUsername] = useState({ value: 'Clocky', error: '' });
+    const [email, setEmail] = useState({ value: 'clocky@gmail.com', error: '' });
+    const [password, setPassword] = useState({ value: 'Password1', error: '' });
     const [oldPassword, setOldPassword] = useState({ value: '', error: '' })
     const [truePassword, setTruePassword] = useState({ value: '', error: '' })
-    const [username, setUsername] = useState({ value: '', error: '' });
     const [inFunction, setInFunction] = useState(false);
+
+    // UserService.getProfile().then(response => {
+    //     try {
+    //         setUsername({ value: response.data.username, error: '' });
+    //         setEmail({ value: response.data.email, error: '' });
+    //         setPassword({ value: response.data.password, error: '' });
+    //     } catch (e) {
+    //         console.log(e);
+    //     }
+    // });
 
     const onEditpressed = () => {
         if (inFunction) {
+            const usernameError = UsernameValidator(username.value);
+            const emailError = EmailValidator(email.value);
             const passwordError = PasswordValidator(password.value);
             const oldPasswordError = PasswordValidator(oldPassword.value)
             const truePasswordError = PasswordValidator(truePassword.value)
-            const usernameError = UsernameValidator(username.value);
-            const emailError = EmailValidator(email.value);
+
             setPassword({...password, error: passwordError});
             setOldPassword({...password, error: oldPasswordError})
             setTruePassword({...password, error: truePasswordError})
             setUsername({...username, error: usernameError});
             setEmail({...email, error: emailError});
-            if (passwordError || usernameError || emailError) {
+
+            if (usernameError || emailError || passwordError || oldPasswordError || truePasswordError) {
+                setUsername({...username, error: usernameError});
+                setEmail({...email, error: emailError});
                 setPassword({...password, error: passwordError});
                 setOldPassword({...oldPassword, error: oldPasswordError})
                 setTruePassword({...truePassword, error: truePasswordError})
-                setUsername({...username, error: usernameError});
-                setEmail({...email, error: emailError});
             } else
+                // UserService.putUser().then(response => {
+                //     try {
+                //         console.log(response);
+                //     } catch (e) {
+                //         console.log(e);
+                //     }
+                // })
                 setInFunction(false);
+                ToastAndroid.showWithGravityAndOffset(
+                    "Profile updated successfully",
+                    ToastAndroid.SHORT,
+                    ToastAndroid.BOTTOM,
+                    25,
+                    50
+                );
         } else
             setInFunction(true);
     }
     return (
         <Background>
             <Ionicons name="person-circle" size={100} />
-            <Header>Connected as ...</Header>
+            <Header>Connected as {username.value}</Header>
 
             <TextInput
                 label="Username"
                 returnKeyType="next"
-                value={UsernameValidator.value}
+                value={username.value}
                 onChangeText={(text) => setUsername({ value: text, error: '' })}
                 error={!!username.error}
                 errorText={username.error}
@@ -62,7 +89,7 @@ export default function Profile() {
             <TextInput
                 label="Email"
                 returnKeyType="next"
-                value={EmailValidator.value}
+                value={email.value}
                 onChangeText={(text) => setEmail({ value: text, error: '' })}
                 error={!!email.error}
                 errorText={email.error}
@@ -76,7 +103,7 @@ export default function Profile() {
                 label="Old Password"
                 returnKeyType="done"
                 value={oldPassword.value}
-                onChangeText={(text) => setPassword({ value: text, error: '' })}
+                onChangeText={(text) => setOldPassword({ value: text, error: '' })}
                 error={!!oldPassword.error}
                 errorText={oldPassword.error}
                 secureTextEntry
@@ -97,7 +124,7 @@ export default function Profile() {
                 label="Confirm Password"
                 returnKeyType="done"
                 value={truePassword.value}
-                onChangeText={(text) => setPassword({ value: text, error: '' })}
+                onChangeText={(text) => setTruePassword({ value: text, error: '' })}
                 error={!!truePassword.error}
                 errorText={truePassword.error}
                 secureTextEntry
@@ -112,6 +139,7 @@ export default function Profile() {
 
 const styles = StyleSheet.create({
     button: {
+        marginTop: 20,
         width: 200,
         height: 50,
         borderRadius: 5,
