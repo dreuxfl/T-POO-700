62516ab9047a84bca1abddce58ea3fb65944143b
dreuxfl <florent.dreux@epitech.eu>
2022-11-15 12:58:05 +0100

    Token persist OK

--- mobile/timemanager/App.js
+++ mobile/timemanager/App.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import {useEffect, useState} from 'react';
 
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
@@ -9,18 +9,31 @@ import Dashboard from "./screens/Dashboard";
 import StartScreen from "./screens/StartScreen";
 import Signup from "./screens/Signup";
 
-import Ionicons from 'react-native-vector-icons/Ionicons';
 import {theme} from "./core/Theme";
+import * as SecureStore from "expo-secure-store";
 
 const Stack = createStackNavigator()
 
 export default function App() {
+    const [token, setToken] = useState(null)
+    useEffect(()=>{
+        const fetchToken = async() => {
+            return(await SecureStore.getItemAsync('access_token'))
+        }
+
+        fetchToken().then(token => {
+            if(token) setToken(token)
+            else setToken(false)
+        });
+    },[]);
+
+    useEffect(()=>{console.log(token)},[token])
     return (
         <Provider theme={theme}>
-            <NavigationContainer>
+            {token !== null && <NavigationContainer>
 
                 <Stack.Navigator
-                    initialRouteName="StartScreen"
+                    initialRouteName={token ? "Dashboard" : "StartScreen"}
                     screenOptions={{
                         headerShown: false,
                     }}
@@ -30,7 +43,7 @@ export default function App() {
                     <Stack.Screen name="Register" component={Signup} />
                     <Stack.Screen name="Dashboard" component={Dashboard} />
                 </Stack.Navigator>
-            </NavigationContainer>
+            </NavigationContainer>}
         </Provider>
     )
 }
