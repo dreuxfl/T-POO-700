fad7557c3dc1f799bd8ad70156e87c70b4e859e4
Sébastien Cartoux <sebastien.cartoux@epitech.eu>
2022-11-15 16:57:51 +0100

    Fixed chart and workingtime in BAND

--- mobile/timemanager/screens/WorkingTime.js
+++ mobile/timemanager/screens/WorkingTime.js
@@ -1,5 +1,3 @@
-
-
 import React, {useEffect, useState} from 'react';
 import { StyleSheet, View, Dimensions } from 'react-native';
 import { LineChart } from "react-native-chart-kit";
@@ -10,74 +8,73 @@ import ChartsManagerService from "../services/ChartsManagerService";
 import moment from "moment";
 import WorkingTimesService from "../services/WorkingTimesService";
 
-
-
 export default function WorkingTime() {
-
-    const [days, setDays] = useState([])
-    const [start, setStart] = useState([])
-    const [end, setEnd] = useState([])
-
-    const [label, setLabel] = useState([])
-    const [hoursWorked, setHoursWorked] = useState([])
-    const [hoursClocked, setHoursClocked] = useState([])
-    const [rows, setRows] = useState([])
+    const [days, setDays] = useState([]);
+    const [start, setStart] = useState([]);
+    const [end, setEnd] = useState([]);
+    const [label, setLabel] = useState([]);
+    const [hoursWorked, setHoursWorked] = useState([]);
+    const [hoursClocked, setHoursClocked] = useState([]);
+    const [rows, setRows] = useState([]);
 
     useEffect(() => {
-
-        const dummy_labels = []
-        const dummy_clocks = []
-        const dummy_workingTimes = []
+        const dummy_labels = [];
+        const dummy_clocks = [];
+        const dummy_workingTimes = [];
 
         ChartsManagerService.getLineChart().then((response) => {
             if (response.data.data.length > 0) {
                 for (let i = 0; i < response.data.data.length; i++) {
                     dummy_labels.push(moment(response.data.data[i].day).format("ddd"));
                     dummy_workingTimes.push(response.data.data[i].workingtime);
-                    dummy_clocks.push(response.data.data[i].hoursclocked)
+                    dummy_clocks.push(response.data.data[i].hoursclocked);
                 }
                 setLabel(dummy_labels);
                 setHoursWorked(dummy_workingTimes);
                 setHoursClocked(dummy_clocks);
-                console.log(dummy_clocks);
-
+                console.log(hoursClocked);
             }
+        }).catch((error) => {
+            console.log(error);
         });
-
     }, []);
 
-
-    useEffect(()=>{
-        const dummy_days = []
-        const dummy_start = []
-        const dummy_end = []
+    useEffect(() => {
+        const dummy_days = [];
+        const dummy_start = [];
+        const dummy_end = [];
 
         WorkingTimesService.getWorkingTimesByUser().then((response) =>{
-            if(response.data.data && response.data.data.length > 0) {
+            if (response.data.data && response.data.data.length > 0) {
                 for (let i = 0; i < response.data.data.length; i++) {
                     dummy_days.push(moment(response.data.data[i].start).format('ddd'));
                     dummy_start.push(moment(response.data.data[i].start).format('HH:mm'));
-                    dummy_end.push(moment(response.data.data[i].end).format('HH:mm'))
+                    dummy_end.push(moment(response.data.data[i].end).format('HH:mm'));
                 }
                 setDays(dummy_days);
                 setStart(dummy_start);
-                setEnd(dummy_end)
-                const dummy_rows = []
-                for (let i = 0; i < days.length; i++) {
-                    dummy_rows.push(
-                        <DataTable.Row key={i}>
-                            <DataTable.Cell>{days[i]}</DataTable.Cell>
-                            <DataTable.Cell numeric>{start[i]}</DataTable.Cell>
-                            <DataTable.Cell numeric>{end[i]}</DataTable.Cell>
-                        </DataTable.Row>
-                    )
-                }
-                setRows(dummy_rows);
+                setEnd(dummy_end);
             }
-        })
+        }).catch((error) => {
+            console.log(error);
+        });
     }, []);
 
-
+    useEffect(() => {
+        if (days.length > 0 && start.length > 0 && end.length > 0) {
+            const dummy_rows = [];
+            for (let i = 0; i < days.length; i++) {
+                dummy_rows.push(
+                    <DataTable.Row key={i}>
+                        <DataTable.Cell>{days[i]}</DataTable.Cell>
+                        <DataTable.Cell numeric>{start[i]}</DataTable.Cell>
+                        <DataTable.Cell numeric>{end[i]}</DataTable.Cell>
+                    </DataTable.Row>
+                );
+            }
+            setRows(dummy_rows);
+        }
+    }, [days, start, end]);
 
     return (
         <View style={styles.viewStyle}>
@@ -88,12 +85,11 @@ export default function WorkingTime() {
                     <DataTable.Title numeric>Start</DataTable.Title>
                     <DataTable.Title numeric>End</DataTable.Title>
                 </DataTable.Header>
-
-                {rows}
+                {rows.length > 0 && rows}
             </DataTable>
             <Header>Your weekly chart</Header>
             {
-                hoursWorked.length > 2 && hoursClocked.length > 2 &&
+                hoursWorked.length > 1 && hoursClocked.length > 1 &&
                 <LineChart
                     bezier
                     data={{
@@ -132,19 +128,16 @@ export default function WorkingTime() {
                     }}
                 />
             }
-
         </View>
     )
-
 }
-
-    const styles = StyleSheet.create({
-        viewStyle: {
-            flex: 1,
-            alignItems: 'center',
-            paddingTop: StatusBar.currentHeight
-        },
-        tableStyle: {
-            width: "96%",
-        },
-    });
+const styles = StyleSheet.create({
+    viewStyle: {
+        flex: 1,
+        alignItems: 'center',
+        paddingTop: StatusBar.currentHeight
+    },
+    tableStyle: {
+        width: "96%",
+    },
+});
